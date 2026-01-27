e0f908552b5fedf839ce0b771b1a163806061a69
Virgile <virgilebatto@gmail.com>
2026-01-27 17:32:33 +0100

    remove collision change

--- src/mjlab/tasks/velocity/config/lerobot_humanoid_full/env_cfgs.py
+++ src/mjlab/tasks/velocity/config/lerobot_humanoid_full/env_cfgs.py
@@ -140,10 +140,10 @@ def lerobot_humanoid_full_rough_env_cfg(play: bool = False) -> ManagerBasedRlEnv
     weight=-1.0,
     params={"sensor_name": self_collision_cfg.name},
   )
-  cfg.scene.terrain.friction = "1.2 0.005 0.0001"
-  cfg.scene.terrain.solref = "0.01 1"
-  cfg.scene.terrain.solimp = "0.99 0.999 0.001 0.5 2"
-  cfg.scene.terrain.contact = "enable"
+  # cfg.scene.terrain.friction = "1.2 0.005 0.0001"
+  # cfg.scene.terrain.solref = "0.01 1"
+  # cfg.scene.terrain.solimp = "0.99 0.999 0.001 0.5 2"
+  # cfg.scene.terrain.contact = "enable"
   # Apply play mode overrides.
   if play:
     # Effectively infinite episode length.
