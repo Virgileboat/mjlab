80af6dc896c11076f6f298941f1e5075a7eaa018
Virgile <virgilebatto@gmail.com>
2026-01-28 09:23:05 +0100

    change contact sensor

--- src/mjlab/tasks/velocity/config/lerobot_humanoid_full/env_cfgs.py
+++ src/mjlab/tasks/velocity/config/lerobot_humanoid_full/env_cfgs.py
@@ -45,8 +45,8 @@ def lerobot_humanoid_full_rough_env_cfg(play: bool = False) -> ManagerBasedRlEnv
   )
   self_collision_cfg = ContactSensorCfg(
     name="self_collision",
-    primary=ContactMatch(mode="subtree", pattern="torso_subassembly_2", entity="robot"),
-    secondary=ContactMatch(mode="subtree", pattern="torso_subassembly_2", entity="robot"),
+    primary=ContactMatch(mode="subtree", pattern="torso_subassembly", entity="robot"),
+    secondary=ContactMatch(mode="subtree", pattern="torso_subassembly", entity="robot"),
     fields=("found",),
     reduce="none",
     num_slots=1,
